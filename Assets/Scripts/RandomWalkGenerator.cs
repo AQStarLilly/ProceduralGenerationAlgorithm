6fd467d9318b84da7eec3076320fb78f6133809c
Kieran Hislop <kieranjhislop@gmail.com>
2025-09-17 09:34:00 -0300

    Finished randomwalk script and set up scene

--- Assets/Scripts/RandomWalkGenerator.cs
+++ Assets/Scripts/RandomWalkGenerator.cs
@@ -6,7 +6,7 @@ public class RandomWalkGenerator : MonoBehaviour
 {
     public int width = 50;
     public int height = 50;
-    public int steps = 150;
+    public int steps = 300;
 
     public GameObject floorPrefab;
     public GameObject wallPrefab;
@@ -23,11 +23,41 @@ public class RandomWalkGenerator : MonoBehaviour
 
     void GenerateLevel()
     {
+        int x = width / 2;
+        int y = height / 2;
 
+        grid[x, y] = true;
+
+        for(int i = 0; i < steps; i++)
+        {
+            int dir = Random.Range(0, 4);
+
+            if (dir == 0 && x > 0) x--;
+            else if (dir == 1 && x < width - 1) x++;
+            else if (dir == 2 && y > 0) y--;
+            else if (dir == 3 && y < height - 1) y++;
+
+            grid[x, y] = true;
+
+        }
     }
     
     void DrawLevel()
     {
-
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Vector3 pos = new Vector3(x, y, 0);
+                if (grid[x, y])
+                {
+                    Instantiate(floorPrefab, pos, Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(wallPrefab, pos, Quaternion.identity);
+                }
+            }
+        }
     }
 }
